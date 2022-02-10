8d17ca35f1ca44ad1d1088d3bf7a7b2d59ea1397
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-02-09 20:34:57 -0500

    recover if single root server fails to snapshot

--- dijon/cli/cli.py
+++ dijon/cli/cli.py
@@ -45,6 +45,7 @@ def run_migrations():
 @click.option("--root-server-id", default=0, show_default=False)
 def run_snapshot(root_server_id: int):
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('dijon.cli.run_snapshot')
     with database.db_context() as db:
         if root_server_id:
             root_server = crud.get_root_server(db, root_server_id)
@@ -56,8 +57,14 @@ def run_snapshot(root_server_id: int):
             root_servers = crud.get_root_servers(db)
 
         for root_server in root_servers:
-            snapshot.create(db, root_server)
-            db.commit()
+            try:
+                snapshot.create(db, root_server)
+            except Exception:
+                # TODO report this somewhere
+                logger.exception(f"error creating snapshot for {root_server.id}:{root_server.url}")
+                db.rollback()
+            else:
+                db.commit()
 
 
 if __name__ == "__main__":
