86e7d3d0279fc4efd6fa8f16ac3b14a5778c7d1b
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-02-09 22:08:14 -0500

    add list formats and list service bodies apis

--- dijon/snapshot/__init__.py
+++ dijon/snapshot/__init__.py
@@ -1,10 +1,10 @@
 from dijon.snapshot.create import create_snapshot
 from dijon.snapshot.diff import diff_snapshots
-from dijon.snapshot.get import get_meetings
+from dijon.snapshot.get import get_formats, get_meetings, get_service_bodies
 
 
 diff = diff_snapshots
 create = create_snapshot
 
 
-__all__ = ["diff", "create", "get_meetings"]
+__all__ = ["diff", "create", "get_formats", "get_meetings", "get_service_bodies"]
--- dijon/snapshot/get.py
+++ dijon/snapshot/get.py
@@ -12,3 +12,17 @@ def get_meetings(db: Session, snapshot_id: int, service_body_bmlt_ids: Optional[
     cache = NawsCodeCache(db, snap.root_server)
     db_meetings = crud.get_meetings_for_snapshot(db, snapshot_id, service_body_bmlt_ids=service_body_bmlt_ids)
     return structs.Meeting.from_db_obj_list(db_meetings, cache)
+
+
+def get_formats(db: Session, snapshot_id: int) -> list[structs.Format]:
+    snap = crud.get_snapshot_by_id(db, snapshot_id)
+    cache = NawsCodeCache(db, snap.root_server)
+    db_formats = crud.get_formats_for_snapshot(db, snapshot_id)
+    return structs.Format.from_db_obj_list(db_formats, cache)
+
+
+def get_service_bodies(db: Session, snapshot_id: int) -> list[structs.ServiceBody]:
+    snap = crud.get_snapshot_by_id(db, snapshot_id)
+    cache = NawsCodeCache(db, snap.root_server)
+    db_service_bodies = crud.get_service_bodies_for_snapshot(db, snapshot_id)
+    return structs.ServiceBody.from_db_obj_list(db_service_bodies, cache)
