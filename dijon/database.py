8a0e704eee9a0c81b118298a19501478df5d9279
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-02-17 09:16:14 -0800

    type hint db_context

--- dijon/database.py
+++ dijon/database.py
@@ -1,8 +1,9 @@
 from contextlib import contextmanager
+from typing import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 from dijon.settings import settings
 
@@ -24,7 +25,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @contextmanager
-def db_context():
+def db_context() -> Generator[Session, None, None]:
     session = SessionLocal()
     try:
         yield session
